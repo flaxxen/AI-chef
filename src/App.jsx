9bccbf0ccbddeeb009f4b10eabce5dea07868d9f
Felix Qvarfordt <felixq@kth.se>
2023-03-28 19:05:40 +0200

    (#5) Removed Vue files
    Removed vue files to clean up the project. Also made some changes and
    added a SearchFormPresenter just to have something to show on the page.
    
    The Presenter is very preliminary but at least does not give us a blank
    page when loading the page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import { defineComponent } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.jsx'
+import HelloWorld from './views/HelloWorldView.jsx'
 
 const App = defineComponent({
     render() {
         return (
             <div>
                 <header>
-                    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
                     <div class="wrapper">
-                    <HelloWorld msg="You did it!" />
-
                     <nav>
                         <RouterLink to="/">Home</RouterLink>
-                        <RouterLink to="/about">About</RouterLink>
                     </nav>
                     </div>
                 </header>
