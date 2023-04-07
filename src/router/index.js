743936d5db5f464710df5d8b43706ccd2fa1c689
senobary <senobary@kth.se>
2023-04-07 07:54:14 +0200

    Login/SignUp skeleton is setup in jsx, next step will be to integrate it with firebase.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '../components/MainPagePresenter.jsx'
+import LoginPage from '../components/LoginPresenter.jsx'
+import SignUpPage from '../components/SignUpPresenter.jsx'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +12,17 @@ const router = createRouter({
       name: 'home',
       component:  MainPage
     },
+    {
+      path: '/login',
+      component:  LoginPage
+    },
+    {
+      path: '/signup',
+      component:  SignUpPage
+    }
   ]
+
+
 })
 
 export default router
