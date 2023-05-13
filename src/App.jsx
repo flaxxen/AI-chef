accf94a38f0bf1bb879ecfcac2754cbac941bcda
Raminking01 <71043231+Raminking01@users.noreply.github.com>
2023-05-13 15:45:10 +0200

    Navbar shows when logged in

--- src/App.jsx
+++ src/App.jsx
@@ -32,27 +32,29 @@ import { RouterLink, RouterView } from 'vue-router'
 import Menu from './components/MenuPresenter';
 import router from './router';
 import SearchModel from './model.js'
-import Navbar from './views/Navbar.jsx';
-const App = defineComponent({
-    render() {
-        return (
-            <div class="container">
-                <Navbar />
-                <div class="mainContent">
-                <header>
-                    <div class="wrapper">
-                    {/* <Menu menuItems={router.getRoutes()}/> */}
-                        <RouterLink to="/Login" style="padding: 0 10px;">Login</RouterLink>
-                        <RouterLink to="/Home" style="padding: 0 10px;">Home</RouterLink>
-                        <RouterLink to="/Favorite" style="padding: 0 10px;">Favorite</RouterLink>
-                    </div>
+import Navbar from './views/NavbarView.jsx';
 
-                </header>
-                <RouterView model={ new SearchModel() } />
-                </div>
+const App = defineComponent({
+  render() {
+    return (
+      <div class="container">
+        <Navbar />
+        <div class="mainContent">
+          <header>
+            <div class="wrapper">
+              <h1>Welcome to the AI Chef!</h1>
+              <p>Customize your meal by choosing your favorite ingredients.</p>
+              {}
+              <RouterLink to="/Login" style="padding: 0 10px;">Login</RouterLink>
+              <RouterLink to="/Home" style="padding: 0 10px;">Home</RouterLink>
+              <RouterLink to="/Favorite" style="padding: 0 10px;">Favorite</RouterLink>
             </div>
-        )
-    }
+          </header>
+          <RouterView model={new SearchModel()} />
+        </div>
+      </div>
+    )
+  }
 })
 
-export default App
+export default App;
