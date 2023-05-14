7b0f12afc65f376cbfb3a35ab3f0e96fe12eba19
Felix Qvarfordt <felixqvarfordt@gmail.com>
2023-05-14 17:27:47 +0200

    Separation of concerns login

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,57 +1,40 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 import { RouterLink } from "vue-router";
 import "/src/assets/login.css";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 
 
 const LoginView = defineComponent({
     props: {
-        email: {
-            type: String,
-        },
-        password: {
-            type: String,
-        },
         onLogin: {
+            type: Function,
+        },
+        onLogout: {
             type: Function,
         },
         errorMessage: {
             type: String,
             default: "", //default state is empty unless something is captured by it
         },
+        updateEmail: {
+            type: Function
+        },
+        updatePassword: {
+            type: Function
+        },
+        user: {
+            type: Object
+        },
     },
 
 
 
     setup(props) {
-
-        const auth = getAuth();
-
-        const email = ref("");
-        const password = ref("");
-        const user = ref(null);
-
-
-        //whenever auth state changes this function will be called and update the user prop
-        onAuthStateChanged(auth, (firebaseUser) => {
-            user.value = firebaseUser;
-        });
-
-        const handleLogin = () => {
-            props.onLogin(email.value, password.value);
-        };
-
-        const handleLogout = () => {
-            signOut(auth)
-                .then(() => {
-
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        };
-
-
+   function emailChanged(event) {
+        props.updateEmail(event.target.value);
+    }
+    function passwordChanged(event) {
+        props.updatePassword(event.target.value);
+    }
 return function render() {
     return (
         <div>
@@ -59,10 +42,10 @@ return function render() {
 
 
             {/*if user.value is not null the first segment will be run, if not the next */}
-            {user.value ? (
+            {props.user.value ? (
                 <div>
-                    <p>You are logged in as {user.value.email}.</p>
-                    <button onClick={handleLogout} className="button">Log Out</button>
+                    <p>You are logged in as {props.user.value.email}.</p>
+                    <button onClick={props.onLogout} className="button">Log Out</button>
                 </div>
             ) : (
 
@@ -75,9 +58,9 @@ return function render() {
                     <div className={"loginError"}>{props.errorMessage && <p className="error">{props.errorMessage}</p>}</div>
 
                 <div className="login">
-                <input type="text" value={email.value} onInput={(x) => email.value = x.target.value} placeholder="Enter E-mail" className="input"/>
-                <input type="password" value={password.value} onInput={(x) => password.value = x.target.value} placeholder="Enter Password" className="input"/>
-                <button onClick={handleLogin} className="button">Login</button>
+                <input type="text" onChange= {emailChanged} onkeypress = {emailChanged} onpaste = {emailChanged} oninput ={emailChanged} placeholder="Enter E-mail" className="input"/>
+                <input type="password" onChange={passwordChanged} onkeypress = {passwordChanged} onpaste = {passwordChanged} oninput ={passwordChanged} placeholder="Enter Password" className="input"/>
+                <button onClick={props.onLogin} className="button">Login</button>
                 <RouterLink to="signup" class="sign">SignUp</RouterLink>
 
                 </div>
