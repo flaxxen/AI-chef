51081bb1117d74d4d96c1af30ae126c6e27f6f5b
Felix Qvarfordt <felix.qvarfordt@gmail.com>
2023-05-14 16:10:23 +0200

    Separation of concerns for recipeview

--- src/views/RecipeView.jsx
+++ src/views/RecipeView.jsx
@@ -1,57 +1,35 @@
-import { defineComponent, ref } from "vue"
-import { getAuth, onAuthStateChanged } from "firebase/auth"
-import { getDatabase, update, ref as dbRef, push, set } from "firebase/database";
-import { useRouter } from 'vue-router'
+import { defineComponent } from "vue"
 import '/src/assets/recipe.css'; // import the css file
 
 const RecipeView = defineComponent({
   props: {
     model: {
       type: Object,
-    }
+    },
+    addFavorite: {
+      type: Function
+    },
+    isAuthenticated: {
+      type: Boolean,
+      default: false
+    },
   },
 
-  setup(props) {
-    const isAuthenticated = ref(false)
-    const auth = getAuth()
-    const router = useRouter()
-    const database = getDatabase()
-    const user = auth.currentUser;
-
-    onAuthStateChanged(auth, (user) => {
-      isAuthenticated.value = !!user
-    })
-
-    function addToFavorites() {
-      /* If the user hasn't logged in, route to the login page. */
-      if (!isAuthenticated.value) {
-        router.push('/login');
-        return;
+  setup(props) { 
+    
+    function addRecipe(event) {
+      props.addFavorite();
+      console.log(props.isAuthenticated);
+      if (props.isAuthenticated) {  
+        event.target.disabled = true;
+        event.target.innerText = 'Already added!';
       }
-
-      event.target.disabled = true;
-
-      console.log(user.uid)
-
-      const recipeRef = push(dbRef(database, `allfavorites`));
-      const recipeId = recipeRef.key;
-      set(recipeRef, {
-        title: props.model.recipe.title,
-        ingredients: props.model.recipe.ingredients,
-        instructions: props.model.recipe.instructions
-      });
-
-      const favoritesRef = dbRef(database, `users/${user.uid}/favorites`);
-      const updates = {};
-      updates[recipeId] = true;
-      update(favoritesRef, updates);
-
-      event.target.innerText = 'Already added!';
     }
 
     function renderIngredients() {
       return props.model.recipe.ingredients.map((ingredient) => <div>{ingredient}</div>);
     }
+
     function renderInstructions() {
       return props.model.recipe.instructions.map((instruction) => <div>{instruction}</div>);
     }
@@ -65,7 +43,7 @@ const RecipeView = defineComponent({
               <ul>
                 <li>
                   <div><h2 className="h2">{props.model.recipe.title}</h2></div>
-                  <button v-if="isAuthenticated" className="button" onClick={addToFavorites}>Add to my favorites</button>
+                  <button v-if={props.isAuthenticated} className="button" onClick={addRecipe}>Add to my favorites</button>
                   <div><h3>Ingredients</h3></div>
                   <div>{renderIngredients()}</div>
                   <div><h3>Instructions</h3></div>
@@ -73,7 +51,10 @@ const RecipeView = defineComponent({
                 </li>
               </ul>
             </div>)
-            : <div></div>)
+            : 
+            <div>
+
+            </div>)
         )
       );
     };
