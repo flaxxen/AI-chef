0ef7ed1a5031264806aba26fe1e55756a1db8edf
Felix Qvarfordt <felixqvarfordt@gmail.com>
2023-05-14 16:10:23 +0200

    Separation of concerns for recipeview

--- src/components/RecipePresenter.jsx
+++ src/components/RecipePresenter.jsx
@@ -1,6 +1,9 @@
-import { defineComponent } from "vue"
+import { defineComponent, ref } from "vue"
 import RecipeView from '../views/RecipeView.jsx'
 
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { getDatabase, update, ref as dbRef, push, set } from "firebase/database";
+import { useRouter } from 'vue-router'
 const Recipe = defineComponent({
   
   props: {
@@ -10,11 +13,44 @@ const Recipe = defineComponent({
   },
 
   setup( props ) {
-    return function render() {
-      return (
-        <RecipeView model={ props.model }/> 
-      );
+    const isAuthenticated = ref(false)
+    const auth = getAuth()
+    const router = useRouter()
+    const database = getDatabase()
+    const user = auth.currentUser;
+
+    onAuthStateChanged(auth, (user) => {
+      isAuthenticated.value = !!user
+    })
+
+    function addToFavorites() {
+      /* If the user hasn't logged in, route to the login page. */
+      if (!isAuthenticated.value) {
+        router.push('/login');
+        return;
+      }
+
+      
+      const recipeRef = push(dbRef(database, `allfavorites`));
+      const recipeId = recipeRef.key;
+      set(recipeRef, {
+        title: props.model.recipe.title,
+        ingredients: props.model.recipe.ingredients,
+        instructions: props.model.recipe.instructions
+      });
+
+      const favoritesRef = dbRef(database, `users/${user.uid}/favorites`);
+      const updates = {};
+      updates[recipeId] = true;
+      update(favoritesRef, updates);
+
     };
+
+      return function render() {
+        return (
+          <RecipeView isAuthenticated={isAuthenticated} addFavorite={addToFavorites} model={ props.model }/> 
+        );
+      };
   },
 });
 
