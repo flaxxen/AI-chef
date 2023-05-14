b58aa117d9c8d4b10fa34600f0e9432b9b24f403
Felix Qvarfordt <felixqvarfordt@gmail.com>
2023-05-14 14:16:58 +0200

    fixed navbar and some other stuff

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.jsx'
 import router from './router'
+import { initializeApp } from "firebase/app";
+import './assets/main.css'
+
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,9 +26,7 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 
-
-import './assets/main.css'
-
+// Import Bootstrap an BootstrapVue CSS files (order is important)
 const app = createApp(App)
 
 app.use(router)
